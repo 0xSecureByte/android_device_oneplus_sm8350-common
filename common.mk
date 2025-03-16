90e2722413b03b81320c99a31746b9fad0170e5d
0xSecureByte <chirantan.code@gmail.com>
2025-03-16 08:59:49 +0530

    sm8350-common: Build missing libs
    Change-Id: I004865ea48cb6274334c4496e96a4934cc08a2fe

--- common.mk
+++ common.mk
@@ -338,6 +338,11 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.radio.poweron_opt=1 \
     persist.vendor.radio.stack_id_1=1
 
+# Servicetracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.1.vendor \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
 # Sensors
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
